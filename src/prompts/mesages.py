201cbfc53171fc0785ab389578e63795596a5580
Yatharth Thakur <thakuryatharth1@gmail.com>
2026-01-06 17:03:51 -0500

    new change in messages

--- src/prompts/mesages.py
+++ src/prompts/mesages.py
@@ -3,3 +3,16 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
+model = ChatOpenAI(model="gpt-4")
+message =  [
+    SystemMessage(content="You are a helpfull assistant"),
+]
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        break
+    message.append(HumanMessage(content=user_input))
+    response = model.invoke(message)
+    message.append(AIMessage(content=response.content))
+    print("AI: ", response.content)
