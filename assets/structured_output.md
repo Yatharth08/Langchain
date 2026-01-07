9a7cd00c80e572087fb20e7ce7c57b2f7e32802e
Yatharth Thakur <thakuryatharth1@gmail.com>
2026-01-06 23:51:38 -0500

    structured output

---
+++ assets/structured_output.md
@@ -0,0 +1,27 @@
+### Structured Output
+A practice of having LLM to return response in well structured format(for eg JSON) rather than free form text.
+
+### Ways to get structured output
+- There are 2 ways:
+    - with_structured_output method
+    - output parser
+
+### with_structured_output
+- This method require a data format.
+- data format can be in TypeDict, pydantic, or json schema
+
+### TypeDict
+- It tell python what key are required and what type of value key should hold.
+- It does not validate data at runtime.
+- It just help with type hints.
+- Lightweight extraction.
+- syntax: summary: Annotated[str, "A brief summary of the product review."]
+
+### Pydantic
+- Validate the type at run time.
+- Type conversion ('age':'32' -> it change string into number 'age':32)
+- It provide built in functionality like EmailStr to validate email (email: EmailStr)
+- It provide Feild function (eg: cgpa: float = Feild(gt=0, lt=10, default, description))
+- It ensure data is structured and type-safe.
+- For production pipelines.
+- syntax: summary: Annotated[str, Field(description="A brief summary of the product review.")]
---
+++ src/structured_output/with_structured_output_pydantic.py
@@ -0,0 +1,20 @@
+from typing import Annotated, List, Literal, Optional
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+model = ChatOpenAI(model="gpt-4")
+
+class Review(BaseModel):
+    summary: Annotated[str, Field(description="A brief summary of the product review.")]
+    key_themes: Annotated[List[str], Field(description="A list of key themes discussed in the review.")]
+    sentiment: Annotated[Literal["pos", "neg"], Field(description="The overall sentiment of the review, either 'pos' for positive or 'neg' for negative.")]
+    pros: Annotated[Optional[List[str]], Field(description="A list of positive aspects mentioned in the review.")]
+    cons: Annotated[Optional[List[str]], Field(description="A list of negative aspects mentioned in the review.")]
+
+structured_model = model.with_structured_output(Review)
+prompt = """
+I have been using the Apple iPhone 13 for a while now and overall I am really happy with it. The phone feels smooth and fast in daily use, the battery easily lasts a full day for me, and the camera takes great photos even in low light. The screen is bright and clear, and iOS feels easy to use. Some downsides are that the screen does not feel as smooth as newer phones with higher refresh rates, charging is a bit slow, and the design has not changed much from older iPhones. But for general everyday use, it is a solid phone that does not let me down.
+"""
+response = structured_model.invoke(prompt)
+print(response)
